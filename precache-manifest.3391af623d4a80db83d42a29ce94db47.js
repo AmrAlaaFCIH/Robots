9c71e5998fb6e9ce81201a904471ce047f416539
Amr Alaa <46508070+AmrAlaaFCIH@users.noreply.github.com>
2019-07-31 05:34:38 +0200

    Robots App

---
+++ precache-manifest.3391af623d4a80db83d42a29ce94db47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83aac1faf02bb58525ea11b01aac30de",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "07358ccc8adaa74cd764",
+    "url": "/Robots/static/css/main.ec09db57.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Robots/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "07358ccc8adaa74cd764",
+    "url": "/Robots/static/js/main.bf7cfc81.chunk.js"
+  },
+  {
+    "revision": "95f51dd4f824221f6d09",
+    "url": "/Robots/static/js/runtime~main.ec77e159.js"
+  }
+]);
